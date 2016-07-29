24aaab5b8858197392a09defe77dbf33b3540d61
swathika5 <swathikakesavan@gmail.com>
2016-07-29 22:46:00 +0530

    Create README.c

---
+++ README.c
@@ -0,0 +1,13 @@
+# oddeven
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    scanf("%d",&num);
+    if(num%2==0)
+    printf("even");
+    else
+        printf("odd");
+	// your code goes here
+	return 0;
+}
